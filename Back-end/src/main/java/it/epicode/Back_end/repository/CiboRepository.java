d713a552c47ac5eb792cfc7e89c1d8e0eea750ef
Gabri0223 <gabrielecare0@gmail.com>
2025-07-16 16:33:06 +0200

    Corretto ciboRepository

--- Back-end/src/main/java/it/epicode/Back_end/repository/CiboRepository.java
+++ Back-end/src/main/java/it/epicode/Back_end/repository/CiboRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CiboRepository extends JpaRepository<Cibo,Long> {
-    @Query("SELECT c FROM Cibo WHERE " +
-    "LOWER (c.nome) LIKE LOWER (CONCAT('%' ,:keyword ,'%')) OR " +
+    @Query("SELECT c FROM Cibo c WHERE " +
+    "LOWER (c.nome) LIKE LOWER (CONCAT('%', :keyword ,'%')) OR " +
     "LOWER (c.marca) LIKE LOWER (CONCAT('%', :keyword , '%')) OR " +
-    "LOWER (c.tipoCibo) LIKE LOWER (CONCAT( '%', :keyword '%'))")
-    Page<Cibo> findByKeyWord(String keyWord, Pageable pageable);
+    "LOWER (c.tipoCibo) LIKE LOWER (CONCAT( '%', :keyword ,'%'))")
+    Page<Cibo> findByKeyWord(@Param("keyword") String keyWord, Pageable pageable);
 }
